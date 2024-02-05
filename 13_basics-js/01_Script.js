5f916e668054f4c0e17a50f7809339b5f78a9631
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-05 19:26:42 +0000

    Changes

---
+++ 13_basics-js/01_Script.js
@@ -0,0 +1,16 @@
+// write infomation to the browser console, good for the debubgging purpose
+console.log('Ashish');
+
+// write directly to html page
+document.write('Ashish Singh');
+
+//it is use to alert the user that something has happend.
+alert('welcome to wscube');
+
+// opens up a Confirm/Cancel dialog and returns true/false depending on user click
+confirm('login to ws cube');
+
+//Creates a dialouge for user input
+prompt("Hello","wscube");
+
+// -------------Important function of practical implementation----------------
