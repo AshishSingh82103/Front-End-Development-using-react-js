28d2b950535e6a5a161b6726455b5488b711ea1e
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-06 20:02:19 +0000

    Changes

---
+++ 15_Function-js/01_script.js
@@ -0,0 +1,34 @@
+// --------------Calling user defined function on click event---------------------------
+function greet() {
+    // alert("Yes");
+    // var name = document.getElementById('name').value;
+    // alert(name);
+    var string = "Hello " + name;
+    // document.getElementsByClassName('greet')[0].innerHTML = string;
+
+}
+
+// -----------------------Simple Calculator using java script--------------
+function cal() {
+    var option1 = document.getElementById('first').value;
+    var option2 = document.getElementById('second').value;
+    var ope = document.getElementById('ope').value;
+    //just cehcked for the value getting we use alert
+    // alert(option1);
+    // alert(option2);
+    // alert(ope);
+    if(ope == '+') {
+        var result = parseInt(option1) + parseInt(option2)
+    }
+    if(ope == '-') {
+        var result = parseInt(option1) - parseInt(option2)
+    }
+    if(ope == '*') {
+        var result = parseInt(option1) * parseInt(option2)
+    }
+    if(ope == '/') {
+        var result = parseInt(option1) / parseInt(option2)
+    }
+    // alert(result);
+    document.getElementById('result').value = result;
+}
