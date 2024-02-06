49200e8cf866db30488f4b26e3beeb4ea4b68b61
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-06 16:29:43 +0000

    Changes

---
+++ 14_Opreators-js/01_script.js
@@ -0,0 +1,36 @@
+// --------------------Airthmetic opreators-------------------
+// ---------------Addition--------------
+var x = 7;
+var y = 8;
+var z = x + y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+// -----------Substraction-----------
+var x = 90;
+var y = 8;
+var z = x - y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+// ------------Multiplication--------------
+var x = 7;
+var y = 8;
+var z = x * y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+// -----------------Division---------------
+var x = 17;
+var y = 8;
+var z = x / y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+
+// -----------------Modulus---------------
+var x = 17;
+var y = 8;
+var z = x % y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
