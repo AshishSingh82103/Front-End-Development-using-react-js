57ddb7b46602bf4b9df6466e6279695218235cea
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-06 17:32:07 +0000

    Changes

--- 14_Opreators-js/01_script.js
+++ 14_Opreators-js/01_script.js
@@ -92,5 +92,74 @@ document.getElementById('res').innerHTML = z;
 
 // ------------------------Logical and ternary operators-------------------------
 
+// --------------&&(Logical-opreators)----------------
+var x = 0 //true;
+var y = 1 //true;
+var z = x && y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+// --------------||(Logical-opreators)----------------
+var x = 0 //true;
+var y = 1 //true;
+var z = x || y;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------Ternary opreators------------------
+var x = 5;
+var y = 6;
+var z;
+z = x > y ? "Yes" : "No";
+document.getElementById('res').innerHTML = z;
+
+// -----------------------------Assingment operators-------------------------
+
+//------------------Simple assingment--------------
+var a = 5;
+var z = a;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------------Add and assingment--------------
+var x = 5;
+var z = 6;
+// z = z + a; z = 5 + 6 = 11;
+ z += x;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------------Subtract and assingment--------------
+var x = 8;
+var z = 6;
+// z = z - a; z = 6 - 8 = -2;
+ z -= x;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------------Multiple and assingment--------------
+var x = 8;
+var z = 6;
+// z = z * a; z = 6 * 8 = 48;
+ z *= x;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------------Devide and assingment--------------
+var x = 8;
+var z = 17;
+// z = z / a; z = 17 / 8 = 2.125;
+ z /= x;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
+//------------------Devide and assingment--------------
+var x = 8;
+var z = 17;
+// z = z % a; z = 17 % 8 = 1;
+ z %= x;
+console.log(z);
+document.getElementById('res').innerHTML = z;
+
 
 
