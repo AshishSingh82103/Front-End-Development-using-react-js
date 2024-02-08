b80a86ae0e0537e5408bb7174f5bfe0b85b1e6d5
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-08 10:04:36 +0000

    Changes

--- 17_Date-js/01_script.js
+++ 17_Date-js/01_script.js
@@ -1,1 +1,30 @@
 // ---------------List of method =>Date()-----------------
+var date = new Date();
+function getDate() {
+    document.getElementById('res').innerText = date;
+}
+
+// -----------------Year => getFullYear()----------------
+var date = new Date();
+function year() {
+    document.getElementById('res').innerText = date.getFullYear();
+}
+
+// -----------------Month => getMonth()----------------
+var date = new Date();
+function month() {
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    document.getElementById('res').innerText = month[date.getMonth()];
+}
+
+ // -----------------Hours => getHours()----------------
+ var date = new Date();
+ function hour() {
+    document.getElementById('res').innerText = date.getHours();
+ }
+
+  // -----------------today date => getDate()----------------
+ var date = new Date();
+ function todate() {
+    document.getElementById('res').innerText = date.getDate();
+ }
