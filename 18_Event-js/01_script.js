789e197b861db27c7e02b8e8f0e834b4feb7eb7f
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-08 17:15:02 +0000

    Changes

---
+++ 18_Event-js/01_script.js
@@ -0,0 +1,48 @@
+// ----------------Event--------------------
+// ---------GetElementsBYName--------------
+function getName() {
+    var uName = document.getElementsByName('u_name')[0].value;
+    // console.log(uName);
+    document.getElementsByName('u_name')[0].value = "";
+    document.getElementsByTagName('h1')[0].innerHTML = uName;
+}
+
+// -----------Set interval method in java script------------------
+function test () {
+    setInterval(function(){
+        alert("hello");
+
+    }, 2500)
+}
+
+// ----------Set timeout method in java script----------------------
+function test1() {
+    setTimeout(function() {
+        alert("Hello");
+
+    },2000)
+}
+
+// -----------Digital clock using java script-----------------------------
+function showTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var second = date.getSeconds();
+    var session = "PM";
+    
+    hours < 10 ? "0" + hours : hours;
+    if(hours > 12) {
+        hours = hours - 12;
+    }
+    minutes < 10 ? "0" + minutes : minutes;
+    second < 10 ? "0" + second : second;
+
+    if(hours >= 12) {
+        session = "AM";
+    }
+
+    var time = hours + ":" + minutes + ":" + second + ":" + session;
+    document.getElementsByTagName('h1')[0].innerText = time;
+    setTimeout(showTime, 1000);
+}
