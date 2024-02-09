998b37c22b835af4cd0f0dd8feef141cc8bcc9a0
Ashish Singh <117755948+AshishSingh82103@users.noreply.github.com>
2024-02-09 15:13:08 +0000

    Changes

---
+++ 21_this.js/01_script.js
@@ -0,0 +1,16 @@
+// ------------------------This--------------------
+// var x = this;
+// document.write(x); // [object Window]
+//  var person = {
+//     firstName: "Ashish",
+//     secondName: "Singh",
+//     fullName: function() {
+//        return this.firstName + " " + this.secondName;
+//     }
+//  }
+//  document.write("<h1>" + person.fullName());
+
+//  -----------Other way-------------
+function remove(elem) {
+    elem.style.display = "none";
+}
